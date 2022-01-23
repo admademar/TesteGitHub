b0c1a6b3349fabc889cf606e716a452f8559097d
admademar <admademar@yahoo.com.br>
2022-01-23 00:08:06 -0300

    Alterado

--- projetoGitHub/src/main/java/projetoGitHub/HelloGitHub.java
+++ projetoGitHub/src/main/java/projetoGitHub/HelloGitHub.java
@@ -5,6 +5,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Hub");
 		System.out.println("Estamos Apredendo GitHub");
+		System.out.println("Alterado");
 	}
 
 
